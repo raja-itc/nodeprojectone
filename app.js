aad58c2545b23c41dc4e8dcfb6835bbd82dae1a2
Rajendra <rajendraitchapurapu@gmail.com>
2023-02-16 10:57:53 +0530

    Added login.js and modified the app.js file accordingly to route to login.js

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const mysql = require('mysql')
 var hbs = require('hbs')
 const app = express()
 const port = 3000
+const loginRouter = require('./routes/login')
+
 
 app.set('view engine','hbs')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => { res.render('login')})
+//app.get('/', (req, res) => { res.render('login')})
+
+app.use('/', loginRouter)
 
 app.post('/auth', async function(request, response){
     let username = request.body.username
