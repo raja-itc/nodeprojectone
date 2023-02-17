82b21f40caf91339eefd8cd4d456e380e87857a7
Rajendra <rajendraitchapurapu@gmail.com>
2023-02-17 06:34:22 +0530

    Added few files for db handling and modified the stuff.

--- database/validate.js
+++ database/validate.js
@@ -3,13 +3,14 @@ const mysql = require('mysql2/promise')
 var mydb = require('../database/database.js')
 
 async function validateUser(userName, Password){
-  const connection = await mysql.createConnection({
+  /*const connection = await mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Airtel123$$',
     database: 'nodeproject'
   })
-  connection.connect()
+  connection.connect()*/
+  const connection = mydb.dbConnection();
   const [results, fields] = await connection.execute('SELECT * FROM users WHERE userid = ? AND pass_word = ?', [userName, Password])
   if (results.length > 0)
     return true
